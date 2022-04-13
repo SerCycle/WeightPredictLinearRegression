7b3daae0b9281769d969d81c2184067dcec1c4b5
SerCycle <michelyanuar@gmail.com>
2022-04-13 08:53:05 +0700

    debugging

--- app.py
+++ app.py
@@ -2,24 +2,39 @@ from flask import Flask, render_template, request, jsonify
 import flask
 import joblib
 import numpy as np
-from pip import main
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def WeightPredict():
+    
+    # while First Landing Page
     if request.method == 'GET':
         return render_template("WeightPredictDashboard.html")
+    
+    # After give value
     elif request.method == 'POST':
+        # mengambil value POST method
+        gender = request.form['gender']
+        if gender == "1":
+            gender = "Laki-laki"
+        elif gender == "2":
+            gender = "Perempuan"
+        else:
+            gender ="???"
+        height = request.form['tinggi']
         print(dict(request.form))
         WeightPredict = dict(request.form).values()
         WeightPredict = np.array([float(x) for x in WeightPredict])
+        # Load Model
         model, std_scaler = joblib.load("Model/model.pkl")
         WeightPredict = std_scaler.transform([WeightPredict])
         print(WeightPredict)
         result = model.predict(WeightPredict)
         result = "%.2f" % result
-        return render_template('WeightPredictDashboard.html', result=result)
+        return render_template('WeightPredictDashboard.html', result=result, gender=gender, tinggi=height)
+    
+    # If Something Wrong
     else:
         return "Ada Kesalahan Nich, Debugging lagi yuk..."
 
